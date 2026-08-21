cf8bfb051d2cc03f01fc263509f31d9a69c1f2a9
Maria <maria.koshevaya2808@gmail.com>
2026-08-21 22:07:31 +0200

    Fix modern normalize import

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,7 @@
 
-
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "modern-normalize";
+import "modern-normalize/modern-normalize.css";
 import "./index.css";
 import App from "./components/App/App.tsx";
 
