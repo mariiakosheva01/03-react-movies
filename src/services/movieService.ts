88628736af5c121df41d8efa35f7500fd47c3505
Maria <maria.koshevaya2808@gmail.com>
2026-08-21 21:50:06 +0200

    Complete React Movies app

---
+++ src/services/movieService.ts
@@ -0,0 +1,26 @@
+import axios from "axios";
+import { Movie } from "../types/movie";
+
+interface FetchMoviesResponse {
+  results: Movie[];
+}
+
+const BASE_URL = "https://api.themoviedb.org/3";
+
+const token = import.meta.env.VITE_TMDB_TOKEN;
+
+export const fetchMovies = async (query: string): Promise<Movie[]> => {
+  const response = await axios.get<FetchMoviesResponse>(
+    `${BASE_URL}/search/movie`,
+    {
+      params: {
+        query,
+      },
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  );
+
+  return response.data.results;
+};
